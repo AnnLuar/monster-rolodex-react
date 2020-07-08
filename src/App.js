ec55d89be38815838dde063b25ee91b286eac949
Serafim <ana.serafim.ext@nokia.com>
2020-07-08 13:28:11 +0100

    add a list of monsters to render. use map function and key attribute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,20 +6,31 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Ann"
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: "abc1"
+        },
+        {
+          name: "Dracula",
+          id: "def2"
+        },
+        {
+          name: "Zombie",
+          id: "ghi3"
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: "Hello Allan" })}>Change Text</button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> { monster.name } </h1>
+        ))}
       </div>
-    )
+    );
   }
 }
 
