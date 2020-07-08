f754daf7a9c2c4b05695aeebbd7512458eba5dab
Serafim <ana.serafim.ext@nokia.com>
2020-07-08 17:19:03 +0100

    create a new component: Card-List. show your list names as a grip inside the new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { CardList } from "./components/card-list/card-list.component";
+
 class App extends Component {
   constructor() {
     super();
@@ -13,17 +15,19 @@ class App extends Component {
   componentDidMount() {
     const url = "https://jsonplaceholder.typicode.com/users";
 
-    fetch(url)    
-      .then( response => response.json() )    
-      .then( users => this.setState({ monsters: users }) )    
+    fetch(url)
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> { monster.name } </h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
