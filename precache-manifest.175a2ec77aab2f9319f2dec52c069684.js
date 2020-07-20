b92ff26a0fffbe2f0bc804ae2db93e38fd6be3e1
aserafim <ana.serafim.ext@nokia.com>
2020-07-20 14:56:42 +0100

    Updates

---
+++ precache-manifest.175a2ec77aab2f9319f2dec52c069684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97003af0d2d44b7c71acefc4f28663bd",
+    "url": "/monster-rolodex-react/index.html"
+  },
+  {
+    "revision": "6e9766bbcb3372f1eaca",
+    "url": "/monster-rolodex-react/static/css/main.7a46ed2f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9766bbcb3372f1eaca",
+    "url": "/monster-rolodex-react/static/js/main.a2515eb9.chunk.js"
+  },
+  {
+    "revision": "e4dd72f320065ee1140c",
+    "url": "/monster-rolodex-react/static/js/runtime-main.50c95c02.js"
+  }
+]);
